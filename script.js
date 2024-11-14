4d34fb96948fbde66fdf6b6b7fc4d67774a81381
GAEL PEREIRA <gael.pereira@utp.ac.pa>
2024-11-14 02:40:24 -0500

    js, overlay, secciones, responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function() {
+    let overlay = document.querySelector('.mask');
+    let scrollDistance = window.scrollY;
+
+// opacidad de 0 a 0.7 dependiendo del scroll
+let opacity = scrollDistance / 1000;  // Se puede ajustar el valor para hacer el efecto más o menos pronunciado
+
+// Limitar la opacidad entre 0 y 1
+opacity = opacity > 0.7 ? 0.7 : opacity;
+
+// Aplicar el cambio de opacidad al overlay
+overlay.style.opacity = opacity;
+  });
