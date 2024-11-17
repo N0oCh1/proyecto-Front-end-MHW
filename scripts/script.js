02283b76c341e2e99add2eba3b8f402d5976555b
Ruben Feng <rubenfeng12@gmail.com>
2024-11-17 18:37:03 -0500

    ✨ wip: Obtener los datos de la API y Agregando funcion de rutas

--- scripts/script.js
+++ scripts/script.js
@@ -24,4 +24,53 @@ overlay.style.opacity = opacity;
       menu.style.display = "flex  "
     }
   })
-  
+
+
+// boton menu MONSTRO
+  async function showMonstros() {
+    try{
+      const container= document.getElementById("resultados")
+      container.innerHTML =''
+      const monstros = await GetMonstro();
+      if(monstros){
+        monstros.map(monstro=>{
+          const createdElement = document.createElement("div")
+          createdElement.setAttribute("class", "monstroCard")
+          createdElement.addEventListener('click', function(){
+            Navigate(monstro.idMonstro)
+          })
+
+          const image = document.createElement("img")
+          image.setAttribute("class", "monstroImagenCard")
+          image.setAttribute("src", monstro.imagen.iconUrl)
+
+          const nombre = document.createTextNode(monstro.nombre)
+          createdElement.appendChild(image)
+          createdElement.appendChild(nombre)
+
+          
+
+          container.appendChild(createdElement)
+        })
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  function Navigate (id) {  
+    window.location.href = `/src/Monstros.html?id=${id}`;
+  }
+    
+
+  // funcion para obtener datos de la API
+  async function GetMonstro () {
+    const monstroData = await fetch("https://localhost:7101/monstro",{
+      method:"GET",
+      headers:{
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    }).then(data=>data.json()).then(data=>data)
+    return monstroData
+  }
