d36f67acc7025e5a4f8d52341c8aa31d1857444e
GAEL PEREIRA <gael.pereira@utp.ac.pa>
2024-11-18 07:07:52 -0500

    cosas

--- scripts/script.js
+++ scripts/script.js
@@ -16,12 +16,65 @@ overlay.style.opacity = opacity;
 
 
   document.getElementById("AbrirMenu").addEventListener('click', function() {
-    const menu = document.getElementById("contenedor2")
+    const menu = document.getElementById("contenedor2");
+    const main = document.getElementById("contenedor1")
     if(menu.style.display === "flex"){
       menu.style.display = "none"
+      main.style.display = "flex"
     }
     else{
-      menu.style.display = "flex  "
+      menu.style.display = "flex"
+      main.style.display = "none"
     }
-  })
-  
+  }
+)
+
+
+// boton menu MONSTRO
+  async function showMonstros() {
+    try{
+      const container= document.getElementById("resultados")
+      container.innerHTML =''
+      const monstros = await GetMonstro();
+      if(monstros){
+        monstros.map(monstro=>{
+          const createdElement = document.createElement("div")
+          createdElement.setAttribute("class", "monstroCard")
+          createdElement.addEventListener('click', function(){
+            Navigate(monstro.idMonstro)
+          })
+
+          const image = document.createElement("img")
+          image.setAttribute("class", "monstroImagenCard")
+          image.setAttribute("src", monstro.imagen.iconUrl)
+
+          const nombre = document.createTextNode(monstro.nombre)
+          createdElement.appendChild(image)
+          createdElement.appendChild(nombre)
+
+          
+
+          container.appendChild(createdElement)
+        })
+      }
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+
+  function Navigate (id) {  
+    window.location.href = `/src/Monstros.html?id=${id}`;
+  }
+    
+
+  // funcion para obtener datos de la API
+  async function GetMonstro () {
+    const monstroData = await fetch("https://localhost:7101/monstro",{
+      method:"GET",
+      headers:{
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    }).then(data=>data.json()).then(data=>data)
+    return monstroData
+  }
